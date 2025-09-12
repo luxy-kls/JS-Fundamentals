8a3fb107efb334de714ed0da496834512723be14
Keyman Luxy Sackey <sackeykeyman@gmail.com>
2025-09-12 16:29:57 +0100

    Describing 3 Programming languages

---
+++ 1-multi_languages.js
@@ -0,0 +1,9 @@
+//Writing about 3 different languages
+const aboutC = "C is fun";
+const aboutPython = "Python is cool";
+const aboutJS = "JavaScript is amazing";
+
+// logging all to the console
+console.log(`${aboutC}
+${aboutPython}
+${aboutJS}`);
