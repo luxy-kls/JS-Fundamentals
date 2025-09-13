f1189042468d509df76eaf8dee7fb8c9e1221178
Keyman Luxy Sackey <sackeykeyman@gmail.com>
2025-09-13 13:01:50 +0100

    Using integer input to create a square

---
+++ 8-square.js
@@ -0,0 +1,13 @@
+//Creating squares
+const squareSize = process.argv.slice(2);
+
+if (!/^-?\d+$/.test(squareSize[0])){
+    console.log("Missing size");
+} else {//declaring a while loop
+    let i = 0;
+
+    while (i < squareSize[0]) {
+        console.log("x".repeat(squareSize[0]));
+             i++; 
+    }
+}
