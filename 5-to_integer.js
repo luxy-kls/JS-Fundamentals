176d22add1a702e90d033de17db414aa2fe308af
Keyman Luxy Sackey <sackeykeyman@gmail.com>
2025-09-13 09:59:38 +0100

    Converts the first argument to an integer

---
+++ 5-to_integer.js
@@ -0,0 +1,11 @@
+//A script that prints converts my first argument to an integer
+
+let argInt = process.argv.slice(2);
+
+
+// checking if the first argument is an integer
+if(!/^\d+$/.test(argInt[0])){
+    console.log("Not a number");
+}  else {
+    console.log(`My number: ${argInt[0]}`);
+}
