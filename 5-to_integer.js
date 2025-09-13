5ea6ab9b87c7c622cf1b02e077562c8e6feca023
Keyman Luxy Sackey <sackeykeyman@gmail.com>
2025-09-13 10:11:58 +0100

    Fixed the Bug

--- 5-to_integer.js
+++ 5-to_integer.js
@@ -4,7 +4,7 @@ let argInt = process.argv.slice(2);
 
 
 // checking if the first argument is an integer
-if(!/^\d+$/.test(argInt[0])){
+if(!/^-?\d+$/.test(argInt[0])){
     console.log("Not a number");
 }  else {
     console.log(`My number: ${argInt[0]}`);
